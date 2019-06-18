058bf43029df88011d4516889ed2bd77d2c1c30b
Hughie Mak <hymak@qbssystem.com>
2019-06-18 11:20:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('emit from client', function () {
+        console.log('emit from client');
+        io.emit('emit from server', "Hi");
+    });
+    // socket.broadcast.emit('broadcase from server');
+});
+
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
