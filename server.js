baa8d52cbed6debb2958b448605ff804e2062302
Hughie Mak <hymak@qbssystem.com>
2019-07-17 12:06:53 +0800

    commit

--- server.js
+++ server.js
@@ -243,8 +243,8 @@ function getAllRooms(socket) {
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-// http.listen(port, ip, function () {
-http.listen(3000, function () {
+http.listen(port, ip, function () {
+// http.listen(3000, function () {
     console.log( "server started")
 
 });
